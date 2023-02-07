b97501cf310fcfc2716adc0ec5a6868422edd31e
kkhanzada <124653745+kkhanzada@users.noreply.github.com>
2023-02-07 10:43:08 +0530

    bank

---
+++ bank.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class bank
+{
+int id;
+String name;
+public static void main(String args[])
+{
+bank S1=new bank();
+S1.id=101;
+S1.name="bala";
+System.out.println(S1.id+S1.name);
+}
+}
